c16f04c395adf6de923443448bfa3ac2dfb969a7
Patty <patriciamarie0816@gmail.com>
2025-12-06 21:39:58 +0800

    update for render config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
-const LAN_IP = "192.168.254.107"; // 👈 replace with your computer’s LAN IP
+const LAN_IP = "192.168.254.107"; 
 const PORT = 5000;
 
 export const API_URL = __DEV__
   ? `http://${LAN_IP}:${PORT}/api` // Development (Expo Go on phone)
-  : "https://your-production-url.com/api"; // Replace with deployed backend URL
+  : "https://managementsystem-backend.onrender.com";
